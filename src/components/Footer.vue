30029ae5c5f732524acfdfd323e52b19d2846f7f
fouziabenadida <benadiafouzia@gmail.com>
2022-09-02 18:53:12 +0300

    first commit

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,56 +1,83 @@
 <template>
-    <v-footer
-      dark
-      padless
-    >
-      <v-card
-        class="flex"
-        flat
-        tile
-      >
-        <v-card-title class="teal">
-          <strong class="subheading">Get connected with us on social networks!</strong>
-  
-          <v-spacer></v-spacer>
-  
-          <v-btn
-            v-for="icon in icons"
-            :key="icon"
-            class="mx-4"
-            dark
-            icon
-          >
-            <v-icon size="24px">
-              {{ icon }}
-            </v-icon>
-          </v-btn>
-        </v-card-title>
-  
-        <v-card-text class="py-2 white--text text-center">
-          {{ new Date().getFullYear() }} — <strong>Fouzia.B</strong>
-        </v-card-text>
-      </v-card>
-    </v-footer>
-  </template>
+  <v-footer dark padless>
+    <v-card flat tile class="black white--text py-12 px-5" width="100%">
+      <v-row>
+        <v-col cols="12" xs="12" sm="6" md="4" class="pa-16">
+          <v-toolbar color="transparent" flat>
+            <v-badge color="#D5F0DB" dot>
+              <v-img src="1.png" contain width="30"></v-img>
+            </v-badge>
 
+            <v-toolbar-title class="ml-4">
+              <span class="green--text">e</span><strong>Food-Delivery</strong>
+            </v-toolbar-title>
+          </v-toolbar>
+          <v-card-text class="pt-0">
+            <v-btn
+              v-for="icon in icons"
+              :key="icon"
+              class="mx-1 white--text"
+              icon
+              fab
+              outlined
+              small
+            >
+              <v-icon size="20px">
+                {{ icon }}
+              </v-icon>
+            </v-btn>
+          </v-card-text>
+        </v-col>
+        <v-col cols="12" xs="12" sm="6" md="2">
+          <v-card-text class="white--text pt-0">
+            <h3>About</h3>
+          </v-card-text>
+          <v-card-text class="grey--text"> How it Work</v-card-text>
+          <v-card-text class="grey--text mt-n4"> Our Promrams </v-card-text>
+          <v-card-text class="grey--text mt-n4"> Promotions </v-card-text>
+          <v-card-text class="grey--text mt-n4"> Refer A Friend </v-card-text>
+        </v-col>
+        <v-col cols="12" xs="12" sm="6" md="2">
+          <v-card-text class="white--text pt-0">
+            <h3>Dellvery</h3>
+          </v-card-text>
+          <v-card-text class="grey--text"> Cost on Delevery </v-card-text>
+          <v-card-text class="grey--text mt-n4"> Payment Method </v-card-text>
+          <v-card-text class="grey--text mt-n4"> Delevery Astus </v-card-text>
+          <v-card-text class="grey--text mt-n4"> Returns </v-card-text>
+        </v-col>
+        <v-col cols="12" xs="12" sm="6" md="4">
+          <v-card-text class="white--text pt-0">
+            <h3>Contact</h3>
+          </v-card-text>
+          <v-card-text class="grey--text"> Fouzia.B </v-card-text>
+          <v-card-text class="grey--text mt-n4">
+         İstanbul Ataşehir
+          </v-card-text>
+          <v-card-text class="grey--text mt-n4"> benadidafouzia@gmail.com </v-card-text>
+          <v-card-text class="grey--text mt-n4"> +90 555 153 67 20 </v-card-text>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-footer>
+</template>
 
-  <script>
-  export default {
-    name: 'FooterView',
-     data ()  {
-        return {
-            icons: [
-        'mdi-facebook',
-        'mdi-twitter',
-        'mdi-linkedin',
-        'mdi-instagram',
-      ],
-        }
-     
+<script>
+export default {
+  name:'FooterView',
+  data ()  {
+    return {
+      icons: [
+      "fab fa-instagram",
+      "fab fa-github",
+      "fab fa-linkedin",
+      "fab fa-facebook-f",
+    ],
     }
-    
-  }
-  </script>
-  <style scoped>
-  
-  </style>
+   
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,26 +10,61 @@
         </v-toolbar>
         <Products />
         <v-card flat color="#FAFAFA" class="mt-8 py-5 px-16">
-            <div
-              style="
-                position: absolute;
-                margin-left: auto;
-                margin-right: auto;
-                left: 0;
-                right: 0;
-                text-align: center;
-              "
-            >
-              <h3>What Our Clients Say</h3>
-            </div>
-            <Clients/>
-          </v-card>
-          <v-toolbar flat color="transparent" class="mt-8">
-            <v-toolbar-title class="text-h6"
-              >Our Trusted Partner</v-toolbar-title
-            >
-          </v-toolbar>
-          <Partner/>
+          <div
+            style="
+              position: absolute;
+              margin-left: auto;
+              margin-right: auto;
+              left: 0;
+              right: 0;
+              text-align: center;
+            "
+          >
+            <h3>What Our Clients Say</h3>
+          </div>
+          <Clients />
+        </v-card>
+        <v-toolbar flat color="transparent" class="mt-8">
+          <v-toolbar-title class="text-h6">Our Trusted Partner</v-toolbar-title>
+        </v-toolbar>
+        <Partner />
+        <v-card
+          flat
+          color="#E2F2E5"
+          class="rounded-xl mtop mbottom mx-10 pa-4"
+          height="350"
+        >
+          <v-row>
+            <v-col cols="12" xs="12" sm="12" md="5">
+              <v-card
+                class="rounded-xl  mobile"
+                outlined
+                flat
+                height="350"
+                width="200"
+              >
+                <v-img src="phone.png" height="350" ></v-img>
+              </v-card>
+            </v-col>
+            <v-col cols="12" xs="12" sm="6" md="7">
+              <h1 class="mt-16">Download App</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.<br />
+                Provident sequi esse tempore maiores quasi quia non.<br />
+                Itaque laboriosam dicta nostrum accusantium assumenda,<br />
+                vitae ab ut vel tenetur minima natus commodi!
+              </p>
+              <v-btn color="black" dark class="mb-10">
+                <v-icon color="white" left>fab fa-apple</v-icon>
+                App Store
+              </v-btn>
+              <v-btn color="black" dark class="ml-4 mb-10">
+                <v-icon color="white" left>fab fa-google-play</v-icon>
+                Play Store
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-card>
       </div>
     </div>
     <Footer />
@@ -40,8 +75,8 @@
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import Products from "../components/Products";
-import Clients from '../components/Clients'
-import Partner from '../components/Partner'
+import Clients from "../components/Clients";
+import Partner from "../components/Partner";
 
 export default {
   name: "HomeVue",
@@ -51,7 +86,7 @@ export default {
     Footer,
     Products,
     Clients,
-    Partner
+    Partner,
   },
 };
 </script>
